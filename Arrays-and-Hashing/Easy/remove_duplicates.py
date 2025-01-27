2c35604874fdc538f2cca64244fc61d0444c361e
Joshua Jackson <actionjayjax4@gmail.com>
2025-01-27 15:08:19 -0600

    Solution for 'Remove Duplicates' problem

--- Arrays-and-Hashing/Easy/remove_duplicates.py
+++ Arrays-and-Hashing/Easy/remove_duplicates.py
@@ -1,8 +1,12 @@
+# Joshua Jackson
 # Problem: 26. Remove Duplicates from Sorted Array
-# url: https://leetcode.com/problems/remove-duplicates-from-sorted-array/?envType=problem-list-v2&envId=array
+# url: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 # Language: Python
+# Difficulty: Easy
 
 """ 
+Description:
+
 Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 
 Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
