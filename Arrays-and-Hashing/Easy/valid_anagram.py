4cd9a61528d522435587b903c288b2c57acc0ee6
Joshua Jackson <actionjayjax4@gmail.com>
2025-02-06 19:53:49 -0600

    Solution for 'Valid Anagram' problem

---
+++ Arrays-and-Hashing/Easy/valid_anagram.py
@@ -0,0 +1,54 @@
+# Joshua Jackson
+# Problem: 242. Valid Anagram
+# url: https://leetcode.com/problems/valid-anagram/description/
+# Language: Python
+# Difficulty: Easy
+
+"""
+Description:
+Given two strings s and t, return true if t is an 
+anagram
+ of s, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false
+
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ 
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+"""
+
+"""*************** SOLUTION ***************"""
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = [0] * 26
+        
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+        
+        for num in count:
+            if num != 0:
+                return False
+        return True
