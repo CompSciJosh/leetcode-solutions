2d28b5d4f34d6895929476c27aa677719b947c1e
Joshua Jackson <actionjayjax4@gmail.com>
2025-02-13 20:15:44 -0600

    Updated solution for 'Remove Duplicates' problem

--- Arrays-and-Hashing/Easy/remove_duplicates.py
+++ Arrays-and-Hashing/Easy/remove_duplicates.py
@@ -7,12 +7,15 @@
 """ 
 Description:
 
-Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once.
+The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 
 Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
 
-Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially.
+The remaining elements of nums are not important as well as the size of nums.
 Return k.
+
 Custom Judge:
 
 The judge will test your solution with the following code:
@@ -53,6 +56,8 @@ nums is sorted in non-decreasing order.
 
 """***************SOLUTION***************"""
 
+from typing import List
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         k = 1
@@ -63,3 +68,12 @@ class Solution:
                 k += 1
         return k
         
+sorted_nums = [0,0,1,1,1,2,2,3,3,3,4,4]
+
+solution = Solution()
+
+num_of_unique_elements = solution.removeDuplicates(sorted_nums)
+
+txt = f"There are {num_of_unique_elements} unique elements.\nThe first {num_of_unique_elements} elements of the following array are unique: {sorted_nums}"
+
+print(txt)
