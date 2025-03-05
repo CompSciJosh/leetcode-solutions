e35e445b23e2f648262151e5ff7a6ceb6e6867e2
Joshua Jackson <actionjayjax4@gmail.com>
2025-03-05 14:07:31 -0600

    Added some of my thoughts to LeetCode #20 - Valid Parentheses.

--- Arrays-and-Hashing/Easy/valid_parentheses.py
+++ Arrays-and-Hashing/Easy/valid_parentheses.py
@@ -40,7 +40,7 @@ class Solution:
         close_to_open = {")": "(", "]" : "[", "}" : "{"} # dictionary - key:value pairs
 
         for c in s: # for every char in the string
-            if c in close_to_open: # if the char, which is a key, is in the dictionary close_to_open
+            if c in close_to_open: # reads keys ... if the char, which is a key, is in the dictionary close_to_open
                 """ if the stack is not empty and the value at the top of the stack 
                     is equivalent to the value of the key:value pair"""
                 if stack and stack[-1] == close_to_open[c]: # passing in the closing symbol and getting the open symbol
@@ -55,13 +55,44 @@ class Solution:
 End of Solution
 """
 symbols_1 = "[]{}(){}"
+
 symbols_2 = "({[{}]})}"
+
 solution = Solution()
+
 result = solution.is_valid(symbols_2)
+
 print(result)
 
 """
 Time Complexity: O(n) <- b/c we have to go through each input character, n, once.
 Space Complexity: O(n) <- b/c the stack could be up to the size of the input, n.
 """
-
+"""
+ More thoughts on the problem
+"""
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         stack = []
+#         symbol_map = {"}":"{", ")":"(", "]":"["}
+"""
+ 1. First thought:
+    If the character is in the dictionary, it lets us know its a key (aka a closing symbol) and we will perform so operations on it.
+    But if its not a closing symbol we will push/append it to the stack.
+ 2. Second thought, operations to be performed:
+    If the stack is not empty and the value at the top of the stack evaluates to the value the c is mapped to in the dictionary then we can pop the stack.
+    Else we can return False because the value at the top of the stack does not evaluate to the value the c is mapped to in the dictionary.
+ 3. Final thought:
+    After all c in s have been visited, we can return True if the stack is empty else False.
+"""
+        # for c in s:
+        #     if c in symbol_map:
+        #         # perform operations
+        #         if stack and stack[-1] == symbol_map[c]:
+        #             stack.pop()
+        #         else:
+        #             return False
+        #
+        #     else:
+        #         stack.append(c)
+        # return True if not stack else False
